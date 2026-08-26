9c3863899ff0bbe5a177e54ae09ee4afc1d6dd0d
Zenas Alpha MT <mtoumamt63@gmail.com>
2026-08-26 18:14:24 +0100

    installation et configuration de firebase initiale

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'pages/home/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,);
   runApp(
     const ProviderScope(
       child: MyApp(),
